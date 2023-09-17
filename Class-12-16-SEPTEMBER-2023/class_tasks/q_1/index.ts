9f15ddc38ceda69695ef243aa8f2530ed0f95874
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2023-09-17 00:22:14 -0700

    class 12 class task q2

--- Class-12-16-SEPTEMBER-2023/class_tasks/q_1/index.ts
+++ Class-12-16-SEPTEMBER-2023/class_tasks/q_1/index.ts
@@ -5,11 +5,35 @@ async function userInput() {
 		{
 			name: "num1",
 			type: "number",
-			message: "Enter the first number?",
+			message: "Enter a number?",
 		},
 	])
-	console.log("num1 is", result.num1)
+	try {
+		if (result.num1 > 100 || result.num1 < 0) {
+			console.log("Enter a valid number between 1 to 100");
+		}
+		else {
+			const marks = result.num1;
+			if (marks >= 90) {
+				console.log("A+");
+			} else if (marks >= 80) {
+				console.log("A");
+			} else if (marks >= 70) {
+				console.log("B");
+			} else if (marks >= 60) {
+				console.log("C");
+			} else if (marks >= 50) {
+				console.log("D");
+			} else {
+				console.log("F");
+			}
+		}
+	} catch (error) {
+		console.log("Catch Error is", error)
+	}
+	finally {
+		console.log("Final is print")
+	}
 }
 
 userInput()
-console.log("After async function");
