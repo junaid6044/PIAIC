a898034244f3afa1a3c4254b11788e9767ca0edb
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2023-11-22 03:21:59 -0800

    fix error

--- Quater2/Class-03-19-NOVEMBER-2023/class-task-assignment/src/app/about/page.tsx
+++ Quater2/Class-03-19-NOVEMBER-2023/class-task-assignment/src/app/about/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
       <section className="about-us">
         <div className="row">
           <div className="about-col">
-            <h1>We are the world's largest University</h1>
+            <h1>We are the world&apos;s largest University</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
             <Link href="/" className="hero_btn btn">EXPLORE NOW</Link>
           </div>
