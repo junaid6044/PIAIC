2e581e6b685e111cd098932cc2e34537562d3579
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2023-12-16 09:41:09 -0800

    class 6 assignment

--- Quater2/Class-06-10-DECEMBER-2023/assignment/src/app/components/Button.tsx
+++ Quater2/Class-06-10-DECEMBER-2023/assignment/src/app/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Button(props:any) {
   return (
-    <button onClick={props.click} className="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-full">
+    <button onClick={props.click} className="bg-transparent hover:bg-teal-800 text-teal-800 font-semibold hover:text-white py-2 px-4 border border-teal-800 hover:border-transparent rounded-full">
       {props.title}
     </button>
   )
--- Quater2/Class-06-10-DECEMBER-2023/assignment/src/app/page.tsx
+++ Quater2/Class-06-10-DECEMBER-2023/assignment/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image'
+import React from 'react';
 import { useState } from 'react';
 import Button from './components/Button';
 
@@ -18,8 +19,8 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <ul>
+    <div className='flex flex-col justify-center items-center'>
+      <ul className='flex gap-8 my-3'>
         {names.map((name, index) => (
           <li
             key={index}
@@ -34,9 +35,41 @@ export default function Home() {
       </ul>
 
 
-      <p>Current Name: {names[currentIndex]}</p>
-      <Button click={handlePrevClick} title="Previous"/>
-      <Button click={handleNextClick} title="Next"/>
+      <p>Current Name: <span className='font-bold'>{names[currentIndex]}</span></p>
+      <div className='flex gap-8 my-3'>
+        <Button click={handlePrevClick} title="Previous"/>
+        <Button click={handleNextClick} title="Next"/>
+      </div>
+
+
+      <table className="w-2/5 leading-normal">
+        <thead>
+          <tr className="bg-gray-300">
+            <th className="px-5 py-6 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+              Index
+            </th>
+            <th className="px-5 py-6 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+              Name
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {names.map((name, index) => {
+            return (
+              <React.Fragment key={index}>
+                <tr className={`${index % 2 !== 0 ? "bg-gray-100" : ""} ${index === currentIndex ? "bg-teal-800 font-bold" : ""}`}>
+                  <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                    <p className={`${index === currentIndex ? "text-white" : "text-gray-900"}  whitespace-no-wrap`}>{index + 1}</p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                    <p className={`${index === currentIndex ? "text-white" : "text-gray-900"}  whitespace-no-wrap`}>{name}</p>
+                  </td>
+                </tr>
+              </React.Fragment>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
