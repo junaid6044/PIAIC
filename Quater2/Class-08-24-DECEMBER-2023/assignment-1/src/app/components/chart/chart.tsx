d3d929460f1589d9fe92afe5dd279bb9284a1417
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2023-12-30 03:35:46 -0800

    add rechart in class 8 assignment

--- Quater2/Class-08-24-DECEMBER-2023/assignment-1/src/app/components/chart/chart.tsx
+++ Quater2/Class-08-24-DECEMBER-2023/assignment-1/src/app/components/chart/chart.tsx
@@ -2,20 +2,35 @@ import { ExpenseType } from '@/app/types/commonTypes';
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+const categories = ['Groceries', 'Rent', 'Utilities', 'Entertainment', 'Online Purchases', 'Other'];
 
-export default function chart(props: {expenseData : ExpenseType[]}) {
+export default function Chart(props: { expenseData: ExpenseType[] }) {
+  // Create an array with zero amounts for each category
+  const dataWithZeroAmounts = categories.map(category => ({
+    category,
+    amount: props.expenseData.find(entry => entry.category === category)?.amount || 0,
+  }));
+
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="60%" height={200}>
       <BarChart
-        data={props.expenseData}
-        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        width={500}
+        height={300}
+        data={dataWithZeroAmounts}
+        margin={{
+          top: 5,
+          right: 5,
+          left: 5,
+          bottom: 5,
+        }}
+        barSize={20}
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey="category" scale="point" padding={{ left: 10, right: 10 }} />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="amount" fill="#115e59" />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Bar dataKey="amount" fill="#8884d8" background={{ fill: '#eee' }} />
       </BarChart>
     </ResponsiveContainer>
   );
