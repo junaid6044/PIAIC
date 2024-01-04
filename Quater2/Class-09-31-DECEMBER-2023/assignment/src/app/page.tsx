7795dcdb4069f9b514524bca8d818feee256874c
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2024-01-04 04:32:20 -0800

    class 9 assignment

--- Quater2/Class-09-31-DECEMBER-2023/assignment/src/app/page.tsx
+++ Quater2/Class-09-31-DECEMBER-2023/assignment/src/app/page.tsx
@@ -2,49 +2,59 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="mt-8 grid grid-cols-1 gap-4 px-2 md:grid-cols-2 md:px-16 lg:grid-cols-3 lg:px-8">
-      <div className="col-span-2 group relative mx-2 my-3 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500 hover:scale-110 lg:mx-5" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
-        <div className={`absolute -left-10 -top-10 z-0 h-20 w-20 transition-all duration-700 group-hover:scale-[20]`} style={{ borderRadius: '50%', backgroundColor: 'grey' }}></div>
-        <h1 className="z-10 text-center text-lg font-bold transition-all duration-700 ease-in-out group-hover:text-white">
-          image
-        </h1>
-        <p className="text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out group-hover:text-white">
+    <div className="mt-8 grid grid-cols-1 px-2 md:grid-cols-2 md:px-16 lg:grid-cols-3 lg:px-8">
+      <div className="col-span-2 group relative m-2 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
+        <Image
+          src="/img/18.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+        <p className="absolute left-5 bottom-6 text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out">
           description
         </p>
       </div>
-      <div className="row-span-2 group relative mx-2 my-3 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500 hover:scale-110 lg:mx-5" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
-        <div className={`absolute -left-10 -top-10 z-0 h-20 w-20 transition-all duration-700 group-hover:scale-[20]`} style={{ borderRadius: '50%', backgroundColor: 'grey' }}></div>
-        <h1 className="z-10 text-center text-lg font-bold transition-all duration-700 ease-in-out group-hover:text-white">
-          image
-        </h1>
-        <p className="text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out group-hover:text-white">
+      <div className="row-span-2 group relative m-2 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
+        <Image
+          src="/img/11.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+        <p className="absolute left-5 bottom-6 text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out">
           description
         </p>
       </div>
-      <div className="row-span-2 group relative mx-2 my-3 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500 hover:scale-110 lg:mx-5" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
-        <div className={`absolute -left-10 -top-10 z-0 h-20 w-20 transition-all duration-700 group-hover:scale-[20]`} style={{ borderRadius: '50%', backgroundColor: 'grey' }}></div>
-        <h1 className="z-10 text-center text-lg font-bold transition-all duration-700 ease-in-out group-hover:text-white">
-          image
-        </h1>
-        <p className="text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out group-hover:text-white">
+      <div className="row-span-2 group relative m-2 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
+        <Image
+          src="/img/12.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+        <p className="absolute left-5 bottom-6 text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out">
           description
         </p>
       </div>
-      <div className="group relative mx-2 my-3 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500 hover:scale-110 lg:mx-5" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
-        <div className={`absolute -left-10 -top-10 z-0 h-20 w-20 transition-all duration-700 group-hover:scale-[20]`} style={{ borderRadius: '50%', backgroundColor: 'grey' }}></div>
-        <h1 className="z-10 text-center text-lg font-bold transition-all duration-700 ease-in-out group-hover:text-white">
-          image
-        </h1>
-        <p className="text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out group-hover:text-white">
+      <div className="group relative m-2 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
+        <Image
+          src="/img/23.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+        <p className="absolute left-5 bottom-6 text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out">
           description
         </p>
       </div>
-      <div className="col-span-2 group relative mx-2 my-3 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500 hover:scale-110 lg:mx-5" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
-        <div className={`absolute -left-10 -top-10 z-0 h-20 w-20 transition-all duration-700 group-hover:scale-[20]`} style={{ borderRadius: '50%', backgroundColor: 'grey' }}></div>
-        <h1 className="z-10 text-center text-lg font-bold transition-all duration-700 ease-in-out group-hover:text-white">
-          image
-        </h1>
-        <p className="text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out group-hover:text-white">
+      <div className="col-span-2 group relative m-2 flex min-h-[300px] w-auto cursor-pointer flex-col items-center justify-evenly overflow-hidden rounded-lg transition-all duration-500" style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 14px' }}>
+        <Image
+          src="/img/25.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+        <p className="absolute left-5 bottom-6 text-md z-10 mx-5 text-center font-medium transition-all duration-700 ease-in-out">
           description
         </p>
       </div>
