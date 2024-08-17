70b53f03dbcc5dcaf71de0f4d6b103e8d012d1ac
Muhammad Junaid <muhammadjunaid7015140@gmail.com>
2024-08-17 10:20:16 +0500

    first langchain complete app

--- Quater4/Class-03-11-AUGUST-2024-langchain-intro/class_03_11_august_2024_langchain_intro/main.py
+++ Quater4/Class-03-11-AUGUST-2024-langchain-intro/class_03_11_august_2024_langchain_intro/main.py
@@ -1,1 +1,10 @@
-print("Hello world! welcome in Langchain")
+from langchain_openai import OpenAI # type: ignore
+from dotenv import load_dotenv # type: ignore
+import os
+load_dotenv()
+
+llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+prompt = "Hello how are you? please translate into french"
+response = llm.invoke(prompt)
+print(response)
